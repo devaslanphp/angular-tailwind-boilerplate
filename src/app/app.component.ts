a88daf33065a5c5596866436ef7160c97c37b013
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-10-02 21:59:41 +0100

    Fontawesome integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+ 
+  faFire = faFire;
+
 }
